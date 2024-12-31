5b10222c866b36e223a52829996fe4ae88d2a98a
Chenghong412 <chenghong92@163.com>
2024-12-31 13:48:14 +0800

    Update assembly.map2graph.sh

--- graph pangenome construction/assembly.map2graph.sh
+++ graph pangenome construction/assembly.map2graph.sh
@@ -1,26 +1,6 @@
 #!/bin/sh
-ref=Fielder.chr1A.rename.fa
-minigraph -cxggs -t16 $ref \
-Aikang58/Aikang58.chr1A.rename.fa \
-ArinaLrFor/ArinaLrFor.chr1A.rename.fa \
-Attraktion/Attraktion.chr1A.rename.fa \
-CS2.1/CS2.1.chr1A.rename.fa \
-Jagger/Jagger.chr1A.rename.fa \
-Julius/Julius.chr1A.rename.fa \
-Kariega/Kariega.chr1A.rename.fa \
-Lancer/Lancer.chr1A.rename.fa \
-Landmark/Landmark.chr1A.rename.fa \
-Mace/Mace.chr1A.rename.fa \
-Norin61/Norin61.chr1A.rename.fa \
-Renan/Renan.chr1A.rename.fa \
-Spelt/Spelt.chr1A.rename.fa \
-Stanley/Stanley.chr1A.rename.fa \
-SYMattis/SYMattis.chr1A.rename.fa \
-Zang1817/Zang1817.chr1A.rename.fa \
-Cadenza/Cadenza.chr1A.rename.fa \
-Claire/Claire.chr1A.rename.fa \
-Paragon/Paragon.chr1A.rename.fa \
-Robigus/Robigus.chr1A.rename.fa \
-Weebill1/Weebill1.chr1A.rename.fa  > 22genome.chr1A.gfa
+for i in CS2.1 Aikang58 ArinaLrFor Attraktion Fielder Jagger Julius Kariega Lancer Landmark Mace Norin61 Renan Spelt Stanley SYMattis Zang1817 Cadenza Claire Paragon Robigus Weebill1
+do
+minigraph genomewide.graph.gfa ${i}.genome.fa > ${i}.map2graph.gaf
+done
 
-#this step is performed per chromosome because of the large size of wheat genome.
